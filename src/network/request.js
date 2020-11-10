fffb825e4196c3f8a61d3e84bf39d441360dbbce
chole <1073757099@qq.com>
2020-11-10 22:34:09 +0800

    '1110'

---
+++ src/network/request.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+
+
+export function request(config) {
+  //1.创建axios实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000',
+    timeout: 5000
+  })
+
+  //2.请求拦截
+  // instance.interceptors.request.use(config => {
+  //   //处理拦截的数据
+
+
+  //   //返回数据
+  //   return config
+  // }, err => {
+  //   console.log(err);
+  // })
+
+
+
+  //3.响应拦截
+  instance.interceptors.response.use(res => {
+    //处理拦截的数据
+
+
+
+    //返回数据
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+
+
+  //3.发送真正的网络请求
+  return instance(config)
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,47 @@
 <template>
-  <h2>主页</h2>
+  <div id="home">
+    <nav-bar class="homenav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <h2>{{recommends}}</h2>
+    <h2>-----------</h2>
+    <h2>{{banners}}</h2>
+  </div>
 </template>
 
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomedata} from 'network/home'
+
+
+
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  components: {
+    NavBar
+  },
+  created() {
+    //1.请求多个数据
+    getHomedata().then(msg =>{
+      this.banners = msg.data.banner.list;
+      this.recommends = msg.data.recommend.list;
+    })
+  }
 }
 </script>
 
 
 <style scoped>
-
+  .homenav {
+    /* 引用了 assets/css/basecss内的变量*/
+    background: var(--color-tint);
+    color: #fff;
+  }
 </style>
